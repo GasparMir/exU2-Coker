e03124981a3d453915ea0f41a0e8946de65251f7
J. Gaspar Miranda Lamadrid <89092725+GasparMir@users.noreply.github.com>
2025-12-05 13:38:15 -0600

    update: app.jsx updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [saludo, setSaludo] = useState("");
 
   const enviar = async () => {
-    const res = await fetch("http://operations:8080/api/saludar", {
+    const res = await fetch("http://localhost:8086/api/saludar", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ nombres, apellidos })
